04fc6ab312703f1cd2043ae0cd4723298e178762
dokeshraddha19 <shraddhadoke1999@gmail.com>
2021-08-25 19:13:27 +0530

    new game

---
+++ TicTacToe.java
@@ -0,0 +1,123 @@
+package newplay;
+import java.util.*;
+public class TicTacToe{
+	char tictoc[][] = new char[3][3];
+
+	
+	public static void display(char tictoc[][]) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(tictoc[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	
+	static void replace(char arr[][], char find, char replace) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (arr[i][j] == find) {
+					arr[i][j] = replace;
+					return;
+				}
+			}
+		}
+	}
+
+	
+	public boolean checkForWin() {
+		return (checkForRow() || checkForColomn() || checkForDiagnol());
+	}
+
+	// This function check row, colomn, diagnol for checkForRow, checForDiagnol,
+	// checkForColomn function
+	public boolean check(char c1, char c2, char c3) {
+		return ((c1 == c2) && (c2 == c3));
+	}
+
+	
+	public boolean checkForRow() {
+		for (int i = 0; i < 3; i++) {
+			if (check(tictoc[i][0], tictoc[i][1], tictoc[i][2]) == true)
+				return true;
+		}
+		return false;
+	}
+
+
+	public boolean checkForColomn() {
+		for (int i = 0; i < 3; i++) {
+			if (check(tictoc[0][i], tictoc[1][i], tictoc[2][0]) == true)
+				return true;
+		}
+		return false;
+	}
+
+	
+	public boolean checkForDiagnol() {
+		return ((check(tictoc[0][0], tictoc[1][1], tictoc[2][2]) == true)
+				|| (check(tictoc[0][2], tictoc[1][1], tictoc[2][0]) == true));
+	}
+
+	
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
+		Scanner sc = new Scanner(System.in);
+		// Select Player
+		String player1, player2;
+		char player1mark, player2mark;
+		System.out.print("Enter Player one Name: ");
+		player1 = sc.nextLine();
+		System.out.print("Enter Player two Name: ");
+		player2 = sc.nextLine();
+		// Mark Selection
+		System.out.println(player1 + " Select Your Marker (O or X): ");
+		// System.out.println(name2+" Select Your Marker (O or X): ");
+		player1mark = sc.next().charAt(0);
+		while (player1mark != 'X' && player1mark != 'x' && player1mark != 'O' && player1mark != 'o') {
+			System.out.print("Invalid Input (Select O or X): ");
+			player1mark = sc.next().charAt(0);
+		}
+		if (player1mark == 'X' || player1mark== 'x')
+			player2mark = 'O';
+		else
+			player2mark = 'X';
+		// initialize board
+		int counter = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				game.tictoc[i][j] = Character.forDigit(++counter, 10);
+			}
+		}
+		// Display Board
+		display(game.tictoc);
+
+		// Play
+
+		char input;
+		for (int i = 0; i < 4; i++) {
+			System.out.print(player1 + " Turn: ");
+			input = sc.next().charAt(0);
+			replace(game.tictoc, input, player1mark);
+			display(game.tictoc);
+			System.out.print(player2 + " Turn: ");
+			input = sc.next().charAt(0);
+			replace(game.tictoc, input, player2mark);
+			display(game.tictoc);
+		}
+		System.out.print("User " + player1 + ": ");
+		input = sc.next().charAt(0);
+		replace(game.tictoc, input, player1mark);
+		display(game.tictoc);
+		// Check For Win or Tie
+
+		if (game.checkForWin())
+			System.out.print("We have a Winner");
+		else
+			System.out.print("Match is Draw");
+	}
+	
+	
+
+}
